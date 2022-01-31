71698c4729e7874c6d92af02b20b214d53ae4fe7
delboy96 <danilo.milosevic.36.16@ict.edu.rs>
2022-01-31 15:21:13 +0100

    Created Category and Product Models, Controllers and ImportCsv command

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Http\Response;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Category $category
+     * @return Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Category $category
+     * @return Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateCategoryRequest $request
+     * @param Category $category
+     * @return Response
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Category $category
+     * @return Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateProductRequest $request
+     * @param Product $product
+     * @return Response
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
