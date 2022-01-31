f6f404937b18ec42e8605c08945a208880c99faa
delboy96 <danilo.milosevic.36.16@ict.edu.rs>
2022-01-31 12:56:45 +0100

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './public/assets/js/*.js',
+      // "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
